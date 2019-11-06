93490b57cf018f9cc8f46341dbcb2682377db49a
Antonino Natale <antonio.natale97@hotmail.com>
2019-11-06 19:50:04 +0100

    Update Travis CI

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-sudo apt install -y --no-install-recommends \
-    libgmp3-dev     \
-    libmpc-dev      \
-    libmpfr-dev     \
-    libisl-dev      \
-    texinfo         \
-    bison           \
-    flex
